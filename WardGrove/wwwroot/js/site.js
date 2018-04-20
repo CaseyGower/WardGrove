309189fb11db639b602d8ba901d1e0391f75b6a7
Casey Gower <caseyg@charter.net>
2018-04-20 09:13:19 -0500

    Save before VS does somehting weird again

--- WardGrove/wwwroot/js/site.js
+++ WardGrove/wwwroot/js/site.js
@@ -5,19 +5,29 @@ var size = {
   width: window.innerWidth || document.body.clientWidth,
 }
 
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+//Default Toggle Tag
+var defaultToggle = document.getElementById("defaultNavToggle");
+
+
+window.onload = function(){
+if (size <= 700){
+defaultToggle.classList.add("navbar-default");
+}
+console.log("default");
+}
 
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky && size >= '750') {
+  if (window.pageYOffset >= sticky && size >= 750) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
