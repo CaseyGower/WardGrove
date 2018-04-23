edfbca4d7cbcdf755908e99b71a1832d71894518
Casey Gower <caseyg@charter.net>
2018-04-23 10:16:00 -0500

    Fix JS issue

--- WardGrove/wwwroot/js/site.js
+++ WardGrove/wwwroot/js/site.js
@@ -5,36 +5,6 @@ function myFunction() {
 var size = {
   width: window.innerWidth || document.body.clientWidth,
 }
-<<<<<<< HEAD
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-//Default Toggle Tag
-var defaultToggle = document.getElementById("defaultNavToggle");
-
-
-window.onload = function(){
-if (size <= 700){
-defaultToggle.classList.add("navbar-default");
-}
-}
-
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky && size >= 750) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-=======
 // Get the navbar
 var navbar = document.getElementById("navbar");
 var navContent = document.getElementById("nav-content")
@@ -53,4 +23,3 @@ var navContent = document.getElementById("nav-content")
   console.log(size);
 }
 
->>>>>>> Peer-Review-Updates
