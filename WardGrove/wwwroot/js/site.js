80e5257bc3a61b9fdddbdac0589c24cdba5e3b54
Casey Gower <caseyg@charter.net>
2018-04-23 09:49:32 -0500

    Commit with JS issue - HELP

--- WardGrove/wwwroot/js/site.js
+++ WardGrove/wwwroot/js/site.js
@@ -1,26 +1,23 @@
-﻿
-// first get the size from the window
-// if that didn't work, get it from the body
+﻿$(document).ready(myFunction() );
+
+function myFunction() {
+
 var size = {
   width: window.innerWidth || document.body.clientWidth,
 }
-
-
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky && size >= '750') {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+ if (size <= 750) {
+    navbar.classList.add("navbar-light");
+    navbar.classList.add("mobileNavCSS");
+    navbar.classList.remove("p-0");
+   } 
+   else {
+    navbar.classList.remove("navbar-light");
+    navbar.classList.remove("mobileNavCSS")
   }
   console.log(size);
 }
+
